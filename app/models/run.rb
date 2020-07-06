45997bc563bb707db8afb2466d3bac47229c5d16
andy-aguilar <aaguil3@gmail.com>
2020-07-06 16:05:49 -0400

    built show pages for runs and shoes

--- app/models/run.rb
+++ app/models/run.rb
@@ -5,4 +5,12 @@ class Run < ApplicationRecord
     def run_location_and_date
         return "#{self.location} -- #{self.date.strftime('%m/%d/%Y')}"
     end
+
+    def format_time
+        seconds = self.time % 60
+        minutes = (self.time / 60) % 60
+        hours = self.time / (60 * 60)
+        # return "#{hours}:#{minutes}:#{seconds}"
+        format("%02d:%02d:%02d", hours, minutes, seconds)
+    end
 end
