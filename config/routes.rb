90041ed50c5cd3e8e7afb20ab356008eb50fc442
andy-aguilar <aaguil3@gmail.com>
2020-07-06 14:41:02 -0400

    made show and index views for runners and brands

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :brands, only: [:index, :show]
   resources :shoes, only: [:index, :show]
   resources :runners, only: [:index, :show]
+  resources :runner_shoes, only: [:show]
 
   root "welcome#homepage"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
