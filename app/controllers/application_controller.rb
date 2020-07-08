efa031c1665979ea8ea9965c36d7f77c95d45fdc
mjester93 <m.jester.93@gmail.com>
2020-07-08 14:05:52 -0400

    adding methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+    helper_method :logged_in?
+    helper_method :authorized
 
     def time_converter(time_params)
         hr_secs = time_params["hours"].to_i * 3600
@@ -12,9 +15,12 @@ class ApplicationController < ActionController::Base
     def current_user
         @current_user = User.find_by(id: session[:user_id])
     end
-    private
 
-    # def runner_params(*args)
-    #     params.require(:user['runner']).permit(*args)
-    # end
+    def logged_in?
+        !!@current_user
+    end
+
+    def authorized
+        redirect_to login_path unless logged_in?
+    end
 end
